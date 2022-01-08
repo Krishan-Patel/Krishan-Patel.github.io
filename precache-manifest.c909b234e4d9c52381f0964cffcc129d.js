19737d4a424662f081de5e818b8ff49cbf693023
Krishan-Patel <61567619+Krishan-Patel@users.noreply.github.com>
2022-01-08 12:49:23 -0500

    Updates

---
+++ precache-manifest.c909b234e4d9c52381f0964cffcc129d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9682da451ee1132ff60f713f150b1c25",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "e2ad95ad050eb23bf0a1",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "33a75910325dc8f51110",
+    "url": "/react-frontend-dev-portfolio/static/css/main.aa5744d7.chunk.css"
+  },
+  {
+    "revision": "e2ad95ad050eb23bf0a1",
+    "url": "/react-frontend-dev-portfolio/static/js/2.09596ee2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.09596ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a75910325dc8f51110",
+    "url": "/react-frontend-dev-portfolio/static/js/main.ac003621.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
