eedca114ae27270923978b52a45c6ccd7fd8dff8
Krishan-Patel <61567619+Krishan-Patel@users.noreply.github.com>
2022-01-08 12:34:22 -0500

    Updates

---
+++ precache-manifest.3d1e50808946ea63100a30580067346e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fa3ea38fda48f6e86f41b025017b46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2ad95ad050eb23bf0a1",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "36e9f8acac158a1162ee",
+    "url": "./static/css/main.aa5744d7.chunk.css"
+  },
+  {
+    "revision": "e2ad95ad050eb23bf0a1",
+    "url": "./static/js/2.09596ee2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.09596ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e9f8acac158a1162ee",
+    "url": "./static/js/main.9b597981.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
